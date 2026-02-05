022cfb3d928777a4a9a3c9a0a3b31beeaaea2046
PriyankGupta22 <gsanu8092@gmail.com>
2026-02-05 23:14:59 +0530

    added recentSubmissions feature

--- src/Leetcode.jsx
+++ src/Leetcode.jsx
@@ -23,10 +23,22 @@ const Leetcode = () => {
         })
     }, [])
 
-    console.log(data?.recentSubmissions)    
+    console.log(data?.recentSubmissions)
+    
+    const totalAcceptanceRate = data?.acceptanceRate
+  ? Number(data.acceptanceRate).toFixed(1)
+  : "0.0";
+
+  if (!data) {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-xl">
+      Loading...
+    </div>
+  );
+}
 
   return (
-    <div className = "w-screen h-[4000px] mt-[40px] relative overflow-visible">
+    <div className = "w-screen h-[2400px] mt-[40px] relative overflow-visible flex flex-col items-center">
       <div className = "w-[1327px] h-[220px] absolute left-[100px] flex flex-row gap-[55px]">
         <div className = "w-[700px] h-[220px] flex justify-center flex-col relative bg-slate-900 rounded-[6px] overflow-hidden">
             <div className = "h-[100px] w-[100px] absolute left-[602px] bottom-[133px] z-10 bg-yellow-300 opacity-10 rounded-[5px]"></div>
@@ -35,14 +47,14 @@ const Leetcode = () => {
                 TOTAL PROBLEMS SOLVED
             </div>
             <div className = "h-[60px] w-[200px]  absolute left-[30px] top-[70px] flex flex-row">
-                <div className = "h-[60px] w-[120px]  text-[60px] flex items-center font-semibold text-white">921</div>
-                <div className = "h-[60px] w-[120px]  text-[25px] flex items-center ml-[15px] mt-[10px] font-bold text-gray-500">/ 2000</div>
+                <div className = "h-[60px] w-[120px]  text-[60px] flex items-center font-semibold text-white">{data.solved}</div>
+                <div className = "h-[60px] w-[120px]  text-[25px] flex items-center ml-[15px] mt-[10px] font-bold text-gray-500">/ {data.total}</div>
             </div>
             <div className = "h-[30px] w-[650px]  absolute left-[30px] top-[140px] flex flex-row items-center z-10">
                 <div className = "h-[12px] w-[550px] flex items-center bg-gray-600 rounded-[4px]">
-                    <div className = "h-[12px] bg-white rounded-[4px]" style = {{width: `${(550)*(921/2000)}px`}}></div>
+                    <div className = "h-[12px] bg-white rounded-[4px]" style = {{width: `${(550)*(data.solved/data.total)}px`}}></div>
                 </div>
-                <div className = "h-[30px] w-[40px]  ml-[15px] font-semibold text-[16px] flex items-center mt-[-2px] text-white">{921/20}%</div>
+                <div className = "h-[30px] w-[40px]  ml-[15px] font-semibold text-[16px] flex items-center mt-[-2px] text-white">{data.solved/20}%</div>
             </div>
         </div>
         <div className = "w-[260px] h-[220px] border border-gray-300 flex flex-col justify-center pl-[30px] rounded-[6px] overflow-hidden z-10 bg-white relative">
@@ -54,7 +66,7 @@ const Leetcode = () => {
                 ACCEPTANCE RATE
             </div>
             <div className = "w-[80px] h-[60px] flex items-center text-slate-900 font-bold text-[40px]">
-                42%
+                {totalAcceptanceRate}%
             </div>
         </div>
         <div className = "w-[260px] h-[220px] border border-gray-300 flex flex-col justify-center pl-[30px] rounded-[6px] overflow-hidden z-10 bg-white relative">
@@ -72,9 +84,9 @@ const Leetcode = () => {
       </div>
 
       <div className = "w-[1327px] h-[170px] absolute left-[100px] top-[260px] flex flex-row gap-[55px]">
-        <Level level = "EASY" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-green-900"/>} solved = {300} total = {500} bgColor = "bg-green-50" textColor = "text-green-900" borderColor = "border-green-300 " barColor = "bg-green-900"/>
-        <Level level = "MEDIUM" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-yellow-900"/>} solved = {600} total = {1200} bgColor = "bg-yellow-50" textColor = "text-yellow-900" borderColor = "border-yellow-300" barColor = "bg-yellow-900"/>
-        <Level level = "HARD" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-red-900"/>} solved = {21} total = {300} bgColor = "bg-red-50" textColor = "text-red-900" borderColor = "border-red-300" barColor = "bg-red-900"/>
+        <Level level = "EASY" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-green-900"/>} solved = {data.easy} total = {300} bgColor = "bg-green-50" textColor = "text-green-900" borderColor = "border-green-300 " barColor = "bg-green-900"/>
+        <Level level = "MEDIUM" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-yellow-900"/>} solved = {data.medium} total = {1200} bgColor = "bg-yellow-50" textColor = "text-yellow-900" borderColor = "border-yellow-300" barColor = "bg-yellow-900"/>
+        <Level level = "HARD" icon = {<IoMdCheckmarkCircleOutline className = "h-[30px] w-[30px] text-red-900"/>} solved = {data.hard} total = {300} bgColor = "bg-red-50" textColor = "text-red-900" borderColor = "border-red-300" barColor = "bg-red-900"/>
       </div>
 
       <div className = "w-[1327px] h-[450px] absolute left-[100px] top-[470px] flex flex-row gap-[55px]">
@@ -84,8 +96,16 @@ const Leetcode = () => {
 
     
 
-      <div className = "w-[1327px] h-[3000px] absolute left-[100px] top-[1000px] flex flex-col items-center gap-[20px] border border-black z-50">
-        
+      <div className = "w-[1100px] h-[1250px] absolute left-[200px] top-[1000px] flex flex-col items-center gap-[20px] border border-gray-300 z-50 rounded-[5px] ">
+            <div className = "w-[1100px] h-[85px] flex flex-col items-center justify-center pt-[25px] gap-[10px]">
+                <div className = "w-[1000px] h-[20px] font-extrabold text-[14px] flex items-center pl-[60px]">
+                    RECENT SUBMISSIONS
+                </div>
+                <div className = "w-[1000px] h-[20px] text-gray-600 text-[15px] flex items-center pl-[60px]">
+                    Latest activity on LeetCode
+                </div>
+            </div>
+            <div className = "w-[900px] h-[3px] bg-black"></div>
             {
                 data?.recentSubmissions?.map(item => (
                     <RecentSubmissionBox 
--- src/RecentSubmissionBox.jsx
+++ src/RecentSubmissionBox.jsx
@@ -1,32 +1,69 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 
 const RecentSubmissionBox = ({title, difficulty, acRate, timestamp}) => {
 
-    if(difficulty === "Hard"){
-        var color = "text-red-800"
-        var bgColor = "bg-red-100"
+    const [date, setDate] = useState(0)
+    const [label, setLabel] = useState("")
+
+    let color = "text-gray-700";
+  let bgColor = "bg-gray-100";
+
+  if (difficulty === "Hard") {
+    color = "text-red-800";
+    bgColor = "bg-red-100";
+  } else if (difficulty === "Medium") {
+    color = "text-yellow-800";
+    bgColor = "bg-yellow-100";
+  } else if (difficulty === "Easy") {
+    color = "text-green-800";
+    bgColor = "bg-green-100";
+  }
+
+    const acceptance =
+  acRate !== undefined && acRate !== null
+    ? Number(acRate).toFixed(2)
+    : "0.00";
+
+    useEffect(() => {
+    if (!timestamp) return;
+
+    const now = Date.now()
+    const submissionTime = timestamp * 1000
+
+    const differenceInTime = now - submissionTime
+    const minutes = Math.floor(differenceInTime/(1000 * 60))
+    const hours = Math.floor(differenceInTime/(1000 * 60 * 60))
+    const days = Math.floor(hours/24)
+
+    if(minutes < 60){
+      setDate(minutes)
+      setLabel(" minutes ago")
+    } else if (hours < 24) {
+      setDate(hours)
+      setLabel(" hours ago")
+    }else{
+      setDate(days)
+      setLabel(" days ago")
     }
-    else if(difficulty === "Medium"){
-        var color = "text-blue-800"
-        var bgColor = "bg-yellow-100"
-    }
-    else if(difficulty === "Easy"){
-        var color = "text-green-800"
-        var bgColor = "bg-green-100"
-    }
+  }, [timestamp]);
+
 
   return (
-    <div className = "w-[1000px] h-[100px]  left-[200px] flex items-center justify-center gap-[20px] text-black">
+    <div className = "w-[1000px] h-[90px]  left-[200px] flex items-center justify-center gap-[10px] text-black">
       <div className = "w-[40px] h-[40px]  flex items-center justify-center text-green-700 text-[30px]">
             <IoMdCheckmarkCircleOutline />
       </div>
       <div className = "w-[700px] h-[80px]  flex flex-col justify-center ">
-        <div className = "w-[500px] h-[40px]  flex flex-col justify-center ml-[30px]">{title}</div>
+        <div className = "w-[500px] h-[40px]  flex flex-col justify-center ml-[30px] font-bold ">{title}</div>
         <div className = "w-[700px] h-[40px]  flex flex-row items-center ">
-            <div className = "w-[200px] h-[40px]  flex flex-col  justify-center  ml-[30px]">{timestamp}</div>
-            <div className = "w-[200px] h-[40px]  flex flex-col  justify-center ">{acRate}</div>
+            <div className = "w-[100px] h-[40px]  flex flex-col  justify-center  ml-[30px] text-gray-500 text-[14px]">{date}{label}</div>
+            <div className = "h-[40px] w-[40px] flex items-center justify-center">
+              <div className = "h-[7px] w-[7px] rounded-[7px] bg-gray-500"></div>
+            </div>
+            <div className = "w-[70px] h-[40px]  flex flex-col  justify-center text-gray-500 text-[14px]">{acceptance} %</div>
         </div>
       </div>
       <div className = {`w-[100px] h-[40px] flex items-center justify-center ${color} ${bgColor}`}>{difficulty}</div>
